a8667628ab492f760948b32076d809442c6c0279
sebasfl.14 <sebasfl.14@hotmail.com>
2020-07-01 19:33:25 -0500

    pece3

---
+++ src/main/java/com/redsocial/Pc3Application.java
@@ -0,0 +1,13 @@
+package com.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/redsocial/Pc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
